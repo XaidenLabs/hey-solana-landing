e5ea675cbe0f6f98211d9b66a2e34132427155cd
SWITLYF <alfreddadieng18@outlook.com>
2025-02-03 03:06:03 -0800

    remove commented sections from Footer component for cleaner code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,6 @@ export default function Footer() {
   return (
     <div className="bg-[#161616] py-12">
       <div className="w-11/12 md:w-10/12 m-auto space-y-8 md:space-y-0 flex flex-col md:flex-row justify-between items-center md:items-start">
-        {/* Logo and Call-to-Action Section */}
         <div className="space-y-6 text-center md:text-left w-full md:w-1/2">
           <div className="flex items-center justify-center md:justify-start gap-x-3">
             <img src="/pngs/logo.png" className="w-10 h-10" alt="Logo" />
@@ -25,7 +24,7 @@ export default function Footer() {
           </div>
         </div>
 
-        {/* Navigation Links Section */}
+
         <div className="flex justify-between w-full md:w-auto gap-x-12 text-center md:text-left text-footergrey">
           <ul className="space-y-4">
             <li className="hover:text-white transition">
@@ -60,7 +59,6 @@ export default function Footer() {
         </div>
       </div>
 
-      {/* Footer Bottom Section */}
       <div className="w-11/12 md:w-10/12 m-auto pt-10">
         <hr className="border-footergrey" />
         <div className="pt-5 text-center text-footergrey">
